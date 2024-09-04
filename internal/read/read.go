029f57772dcc4bc113d96743f41da078ae9d1558
aasset <Gontabek@gmail.com>
2024-09-04 18:05:22 +0500

    reader func merge

--- internal/read/read.go
+++ internal/read/read.go
@@ -5,20 +5,19 @@ import (
 	"encoding/binary"
 	"fmt"
 	"io"
-	"log"
 )
 
-func ReadHeader(r io.Reader) (*models.BitmapHeader, error) {
+func ReadImage(r io.Reader) (*models.BitmapHeader, [][]models.Pixel, error) {
 	header := make([]byte, 54) // 14 bytes for file header + 40 bytes for DIB header
 	_, err := r.Read(header)
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
 	// Parse BMP file header
 	fileType := string(header[0:2])
 	if fileType != "BM" {
-		return nil, fmt.Errorf("not a valid BMP file")
+		return nil, nil, fmt.Errorf("not a valid BMP file")
 	}
 
 	fileSize := binary.LittleEndian.Uint32(header[2:6])
@@ -37,21 +36,25 @@ func ReadHeader(r io.Reader) (*models.BitmapHeader, error) {
 	colorsUsed := binary.LittleEndian.Uint32(header[46:50])
 	colorsImportant := binary.LittleEndian.Uint32(header[50:54])
 
-	// Log the extracted values (for debugging purposes)
-	log.Printf("FileType: %s", fileType)
-	log.Printf("FileSize: %d", fileSize)
-	// log.Printf("DataOffset: %d", dataOffset)
-	log.Printf("HeaderSize: %d", headerSize)
-	log.Printf("Width: %d", width)
-	log.Printf("Height: %d", height)
-	log.Printf("Planes: %d", planes)
-	log.Printf("BitsPerPixel: %d", bitsPerPixel)
-	log.Printf("Compression: %d", compression)
-	log.Printf("ImageSize: %d", imageSize)
-	log.Printf("XPixelsPerM: %d", xPixelsPerM)
-	log.Printf("YPixelsPerM: %d", yPixelsPerM)
-	log.Printf("ColorsUsed: %d", colorsUsed)
-	log.Printf("ColorsImportant: %d", colorsImportant)
+	pixels := make([][]models.Pixel, height)
+	for i := height - 1; i >= 0; i-- {
+		row := make([]models.Pixel, 0)
+		for j := 0; j < int(width); j++ {
+			bytes := make([]byte, 3)
+			_, err := r.Read(bytes)
+			if err != nil {
+				return nil, nil, err
+			}
+
+			row = append(row, models.Pixel{
+				Blue:  bytes[0],
+				Green: bytes[1],
+				Red:   bytes[2],
+			})
+		}
+
+		pixels[i] = row
+	}
 
 	return &models.BitmapHeader{
 		FileType:        fileType,
@@ -67,29 +70,5 @@ func ReadHeader(r io.Reader) (*models.BitmapHeader, error) {
 		YPixelsPerM:     int(yPixelsPerM),
 		ColorsUsed:      int(colorsUsed),
 		ColorsImportant: int(colorsImportant),
-	}, nil
-}
-
-func ReadImage(r io.Reader, width, height int) ([][]models.Pixel, error) {
-	pixels := make([][]models.Pixel, height)
-	for i := height - 1; i >= 0; i-- {
-		row := make([]models.Pixel, 0)
-		for j := 0; j < width; j++ {
-			bytes := make([]byte, 3)
-			_, err := r.Read(bytes)
-			if err != nil {
-				return nil, err
-			}
-
-			row = append(row, models.Pixel{
-				Blue:  bytes[0],
-				Green: bytes[1],
-				Red:   bytes[2],
-			})
-		}
-
-		pixels[i] = row
-	}
-
-	return pixels, nil
+	}, pixels, nil
 }
