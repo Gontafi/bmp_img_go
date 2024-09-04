fed1dd28010f2fa0eb64a123351a309de158e79f
tkoszhan <tamutdzhin2006@mail.ru>
2024-09-04 19:27:48 +0500

    Crop function done. Should be checked for bugs

--- internal/apply/crop/crop.go
+++ internal/apply/crop/crop.go
@@ -8,15 +8,21 @@ import (
 
 var ValuesOutOfBound = errors.New("Values out of bound")
 
+// Width and Height may or not be set, if they was not set, then 0 value should be passed to function
 func Crop(pixels [][]m.Pixel, offsetX, offsetY, width, heigth int) ([][]m.Pixel, error) {
-	if offsetX < 0 || offsetY < 0 || offsetX >= len(pixels[0]) || offsetY >= len(pixels) || offsetX+width >= len(pixels[0]) || offsetY+heigth >= len(pixels) {
+	if offsetX < 0 || offsetY < 0 || offsetX >= len(pixels[0]) || offsetY >= len(pixels) || offsetX+width > len(pixels[0]) || offsetY+heigth > len(pixels) || width < 0 || heigth < 0 {
 		return nil, ValuesOutOfBound
 	}
-	var cropped [][]m.Pixel
 
-	cropped = pixels[offsetY : offsetY+heigth]
+	// If width and height was not set cut only by offsets
+	if width == 0 && heigth == 0 {
+		width = len(pixels[0]) - offsetX
+		heigth = len(pixels) - offsetY
+	}
+	var cropped [][]m.Pixel
+	cropped = pixels[offsetY : offsetY+heigth] // Cut by Y dimension
 	for i := 0; i < len(cropped); i++ {
-		cropped[i] = cropped[i][offsetX : offsetX+width]
+		cropped[i] = cropped[i][offsetX : offsetX+width] // Cutting by X dimension
 	}
 
 	return cropped, nil
