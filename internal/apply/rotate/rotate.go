fb40c7bf324f28e01ae16a98af8d20d616521822
aasset <Gontabek@gmail.com>
2024-09-05 16:10:20 +0500

    \'empty\' argument removed

--- internal/apply/rotate/rotate.go
+++ internal/apply/rotate/rotate.go
@@ -9,14 +9,13 @@ func Rotate(image [][]m.Pixel, direction string) ([][]m.Pixel, error) {
 	if len(image) == 0 {
 		return nil, fmt.Errorf("Image lenght error")
 	}
-	empty := [][]m.Pixel{}
 	switch direction {
 	case "right", "90", "-270":
-		return RotateRight(empty, image), nil
+		return RotateRight(image), nil
 	case "180", "-180":
-		return BottomUp(empty, image), nil
+		return BottomUp(image), nil
 	case "270", "left", "-90":
-		return RotateLeft(empty, image), nil
+		return RotateLeft(image), nil
 	case "0", "360", "-360":
 		return image, nil
 	default:
@@ -25,38 +24,41 @@ func Rotate(image [][]m.Pixel, direction string) ([][]m.Pixel, error) {
 	return nil, fmt.Errorf("Error direction")
 }
 
-func RotateRight(empty [][]m.Pixel, image [][]m.Pixel) [][]m.Pixel {
+func RotateRight(image [][]m.Pixel) [][]m.Pixel {
+	var rotatedImage [][]m.Pixel
 	for i := 0; i < len(image[0]); i++ {
 		row := []m.Pixel{}
 		for j := 0; j < len(image); j++ {
 			pixel := image[len(image)-1-j][i]
 			row = append(row, pixel)
 		}
-		empty = append(empty, row)
+		rotatedImage = append(rotatedImage, row)
 	}
-	return empty
+	return rotatedImage
 }
 
-func RotateLeft(empty [][]m.Pixel, image [][]m.Pixel) [][]m.Pixel {
+func RotateLeft(image [][]m.Pixel) [][]m.Pixel {
+	var rotatedImage [][]m.Pixel
 	for i := len(image[0]) - 1; i >= 0; i-- {
 		row := []m.Pixel{}
 		for j := 0; j < len(image); j++ {
 			pixel := image[j][i]
 			row = append(row, pixel)
 		}
-		empty = append(empty, row)
+		rotatedImage = append(rotatedImage, row)
 	}
-	return empty
+	return rotatedImage
 }
 
-func BottomUp(empty [][]m.Pixel, image [][]m.Pixel) [][]m.Pixel {
+func BottomUp(image [][]m.Pixel) [][]m.Pixel {
+	var rotatedImage [][]m.Pixel
 	for i := len(image) - 1; i >= 0; i-- {
 		row := []m.Pixel{}
 		for j := len(image[0]) - 1; j >= 0; j-- {
 			pixel := image[i][j]
 			row = append(row, pixel)
 		}
-		empty = append(empty, row)
+		rotatedImage = append(rotatedImage, row)
 	}
-	return empty
+	return rotatedImage
 }
