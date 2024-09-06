1253f302b3ac2e78f3a54b79f2c38cb0272d39e8
tkoszhan <tamutdzhin2006@mail.ru>
2024-09-06 10:37:25 +0500

    If width and height was not set, -1 value should be passed to function Crop()

--- internal/apply/crop/crop.go
+++ internal/apply/crop/crop.go
@@ -6,16 +6,16 @@ import (
 	m "bitmap/internal/models"
 )
 
-var ValuesOutOfBound = errors.New("Values out of bound")
+var ValuesOutOfBound = errors.New("Given values out of bound")
 
-// Width and Height may or not be set, if they was not set, then 0 value should be passed to function
+// Width and Height may or not be set, if they was not set, then -1 value should be passed to function
 func Crop(pixels [][]m.Pixel, offsetX, offsetY, width, heigth int) ([][]m.Pixel, error) {
-	if offsetX < 0 || offsetY < 0 || offsetX >= len(pixels[0]) || offsetY >= len(pixels) || offsetX+width > len(pixels[0]) || offsetY+heigth > len(pixels) || width < 0 || heigth < 0 {
+	if offsetX < 0 || offsetY < 0 || offsetX >= len(pixels[0]) || offsetY >= len(pixels) || offsetX+width > len(pixels[0]) || offsetY+heigth > len(pixels) || width <= 0 || heigth <= 0 {
 		return nil, ValuesOutOfBound
 	}
 
 	// If width and height was not set cut only by offsets
-	if width == 0 && heigth == 0 {
+	if width == -1 && heigth == -1 {
 		width = len(pixels[0]) - offsetX
 		heigth = len(pixels) - offsetY
 	}
