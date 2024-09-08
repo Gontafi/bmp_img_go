8e56cb6bd4f04cfa0a09b2ff4fb800c4d2ba5b9e
tkoszhan <tamutdzhin2006@mail.ru>
2024-09-08 16:02:19 +0500

    Added Usage print in main

--- main.go
+++ main.go
@@ -4,11 +4,13 @@ import (
 	"os"
 
 	"bitmap/internal/app"
+	"bitmap/pkg"
 )
 
 func main() {
 	err := app.ParseArgsAndRunCommands(os.Args)
 	if err != nil {
+		pkg.PrintUsage()
 		os.Exit(1)
 	}
 }
